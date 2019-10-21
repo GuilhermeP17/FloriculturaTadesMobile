677338c6e425633bc254ce85e8ccd1933bd93050
Guilherme <guisil_ps@hotmail.com>
2019-10-21 17:44:09 -0300

    Ajustando layout main, deixando teste(Hardcode) de listagem

--- Floricultura Tades/app/src/main/java/br/com/senac/projectsolutions/View/MainView.java
+++ Floricultura Tades/app/src/main/java/br/com/senac/projectsolutions/View/MainView.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import br.com.senac.projectsolutions.R;
+
 public class MainView extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
